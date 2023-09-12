d201b696ad99545a76ae31997752e923a9b7e4e4
Giorgi Botsvadze <bocvik@gmail.com>
2023-09-12 08:17:55 +0400

    rewrite old cpp code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,48 +1,76 @@
+/**
+ * One byte communication protocol
+ * -----------------------------
+ * | Command bit |  Data bits  |
+ * -----------------------------
+ * |      1      |   1111111   |
+ * -----------------------------
+ *
+ * READ  - 0 | Data bits unused
+ * WRITE - 1 | bit 0-5 (pin number), bit 6 (0 - LOW, 1 - HIGH)
+ */
+
 #include <ADS1115_WE.h>
 #include <Wire.h>
 
-/**
- * One byte communication protocol
- * ----
- * command bits |  data bits
- * --------------------------
- *    11        |  11 11 11
- *
- * available commands: 2
- * 
- * command READ_SENSORS - 0
- * command WRITE_PIN    - 1
- * 
- * 
- */
-
-// Serial configuration
-#define BAUD_RATE 9600
+#define BAUD_RATE 9600    // 115200, 250000, 500000, 1000000, 2000000, 9600, 57600
+#define WIRE_CLOCK 400000 // Set I2C clock to 400kHz. it is maximum for MEGA2560
 #define I2C_ADDRESS 0x48
 
-// Command configuration
-#define MAX_COMMAND_BUFFER_SIZE 16
-#define COMMAND_BEGIN_MARKER 60 // < þ
-#define COMMAND_END_MARKER 62   // > ÿ
+#define COMMAND_READ 0
+#define COMMAND_WRITE 1
 
-// Available commands
-#define COMMAND_READ_SENSORS 1
-#define COMMAND_WRITE_PINS 2
+#define MULTIPLEXER_PIN 14
+#define COUNTER_PIN_0 16
+#define COUNTER_PIN_1 17
+#define COUNTER_PIN_2 18
+#define COUNTER_PIN_3 19
 
-// Loop state
-#define STATE_AWAITING_COMMAND 0
-#define STATE_READING_COMMAND 1
-#define STATE_EXECUTING_COMMAND 2
+#define VOLTAGE_MULTIPLIER 5
+#define NUMBER_OF_VOLTAGES_PER_CHANNEL 16
+#define NUMBER_OF_VOLTAGE_CHANNELS 3
 
-// Initial state
-byte state = STATE_AWAITING_COMMAND;
+ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
-// Command buffer
-byte commandSize = 0;
-byte commandBuffer[MAX_COMMAND_BUFFER_SIZE];
+bool isPinWritable(byte pin)
+{
+  return (2 <= pin && pin <= 13) || (22 <= pin <= 53);
+}
+
+float measureVoltage(ADS1115_MUX channel)
+{
+  adc.setCompareChannels(channel);
+  adc.startSingleMeasurement();
+  while (adc.isBusy())
+  {
+    // Do nothing
+  }
+  return adc.getResult_V() * VOLTAGE_MULTIPLIER; // use getResult_mV for millivolts
+}
 
 void setup()
 {
+  // set particular pins as outputs
+  // pinMode(13, OUTPUT); // heart bit generator
+  pinMode(MULTIPLEXER_PIN, OUTPUT);
+  pinMode(15, OUTPUT); // Spare
+
+  pinMode(COUNTER_PIN_0, OUTPUT);
+  pinMode(COUNTER_PIN_1, OUTPUT);
+  pinMode(COUNTER_PIN_2, OUTPUT);
+  pinMode(COUNTER_PIN_3, OUTPUT);
+
+  for (byte pin = 2; pin < 53; pin++)
+  {
+    if (isPinWritable(pin))
+    {
+      pinMode(pin, OUTPUT);
+    }
+  }
+
+  Wire.begin();
+  Wire.setClock(WIRE_CLOCK);
+  Serial.setTimeout(500); // set new value to 1 milliseconds
   Serial.begin(BAUD_RATE);
 }
 
@@ -50,67 +78,48 @@ void loop()
 {
   if (Serial.available() > 0)
   {
-    byte x = Serial.read();
-    if (x == COMMAND_BEGIN_MARKER)
+    byte input = Serial.read();
+    byte command = (input & 0b10000000) >> 7;
+    switch (command)
     {
-      String str = Serial.readStringUntil(COMMAND_END_MARKER);
-      Serial.println(str);
-      for (byte i = 0; i < commandSize; i++)
-      {
-        Serial.print(commandBuffer[i]);
-        Serial.print(" ");
-      }
-
-      Serial.println("");
-    }
-  }
-  // awaitCommand();
-  // executeCommand();
-}
-
-void awaitCommand()
-{
-  if (state == STATE_AWAITING_COMMAND || state == STATE_READING_COMMAND)
-  {
-    if (Serial.available() > 0)
-    {
-      int x = Serial.readBytesUntil('g', commandBuffer, MAX_COMMAND_BUFFER_SIZE);
-
-      if (x == COMMAND_BEGIN_MARKER)
-      {
-        commandSize = 0;
-        state = STATE_READING_COMMAND;
-      }
-
-      if (state == STATE_READING_COMMAND)
-      {
-        commandBuffer[commandSize] = x;
-        commandSize += 1;
-
-        if (commandSize == MAX_COMMAND_BUFFER_SIZE && x != COMMAND_END_MARKER)
-        {
-          commandSize = 0;
-          state = STATE_AWAITING_COMMAND;
-        }
-
-        if (x == COMMAND_END_MARKER)
-        {
-          state = STATE_EXECUTING_COMMAND;
-        }
-      }
+    case COMMAND_READ:
+      read(input);
+      break;
+    case COMMAND_WRITE:
+      write(input);
+      break;
     }
   }
 }
 
-void executeCommand()
+void read(byte input)
 {
-  if (state == STATE_EXECUTING_COMMAND)
+  byte numberOfVoltages = NUMBER_OF_VOLTAGES_PER_CHANNEL * NUMBER_OF_VOLTAGE_CHANNELS;
+  float voltages[numberOfVoltages];
+  for (byte i = 0; i < NUMBER_OF_VOLTAGES_PER_CHANNEL; i++)
   {
-    for (byte i = 0; i < commandSize; i++)
-    {
-      Serial.print(commandBuffer[i]);
-    }
-    commandSize = 0;
-    state = STATE_AWAITING_COMMAND;
+    digitalWrite(COUNTER_PIN_0, (i & 0b0001));
+    digitalWrite(COUNTER_PIN_1, (i & 0b0010) >> 1);
+    digitalWrite(COUNTER_PIN_2, (i & 0b0100) >> 2);
+    digitalWrite(COUNTER_PIN_3, (i & 0b1000) >> 3);
+
+    voltages[i] = measureVoltage(ADS1115_COMP_0_GND);
+    voltages[i + 16] = measureVoltage(ADS1115_COMP_1_GND);
+    voltages[i + 32] = measureVoltage(ADS1115_COMP_2_GND);
+  }
+
+  for (byte i = 0; i < numberOfVoltages; i++)
+  {
+    Serial.write(voltages[i]); // float is 4 bytes
+  }
+}
+
+void write(byte input)
+{
+  byte pin = (input & 0b01111110) >> 1;
+  if (isPinWritable(pin))
+  {
+    byte val = (input & 0b00000001);
+    digitalWrite(pin, val);
   }
 }
